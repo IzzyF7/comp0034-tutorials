2b85089c19a9b29d280ec1dd61dde61c8a2d1a15
Isabelle Fitzpatrick <uclqgfi@ucl.ac.uk>
2025-01-29 17:06:51 +0000

    tutorial 3

--- src/student/dash_single/paralympics_dash.py
+++ src/student/dash_single/paralympics_dash.py
@@ -6,6 +6,7 @@ from student.dash_single.figures import bar_gender
 from student.dash_single.figures import scatter_geo
 from student.dash_single.figures import para_card
 from student.dash_single.figures import country_hist
+from dash import Input, Output
 
 # Variable that defines the meta tag for the viewport
 meta_tags = [
@@ -61,10 +62,6 @@ app.layout = dbc.Container([
             width=6
         ),
         dbc.Col(
-            dcc.Graph(id='bar_chart', figure=bar_fig),
-            width=6
-        ),
-        dbc.Col(
             dcc.Graph(id='map', figure=map),
             width=12
         ), 
@@ -73,9 +70,48 @@ app.layout = dbc.Container([
             width=12
         )
     ]),
+    dbc.Row([
+        dbc.Col(
+            dcc.Graph(id='bar_chart', figure=bar_fig),
+            width=6
+        )
+    ]),
     dbc.Col(children=[card], id='card', width=4),
 ])
 
+
+@app.callback(
+    Output('line-chart', 'figure'),  # The component being updated is the line-chart with id="line-chart"
+    Output('bar_chart', 'figure'),  # The component being updated is the bar_chart with id="bar_chart"
+    Input('dropdown-input', 'value'),  # The input is the dropdown with id="dropdown-input"
+    Input('checklist-input', 'value')  # The input is the checklist with id="checklist-input"
+)
+def update_charts(dropdown_value, checklist_value):
+    # Update line chart based on dropdown value
+    if dropdown_value == "events":
+        line_fig = line_chart("events")
+    elif dropdown_value == "sports":
+        line_fig = line_chart("sports")
+    elif dropdown_value == "countries":
+        line_fig = line_chart("countries")
+    elif dropdown_value == "participants":
+        line_fig = line_chart("participants")
+    
+    # Update bar chart based on checklist value
+    if "summer" in checklist_value and "winter" in checklist_value:
+        bar_fig_summer = bar_gender("Summer")
+        bar_fig_winter = bar_gender("Winter")
+        return line_fig, bar_fig_summer, bar_fig_winter
+    elif "summer" in checklist_value:
+        bar_fig = bar_gender("Summer")
+        return line_fig, bar_fig
+    elif "winter" in checklist_value:
+        bar_fig = bar_gender("Winter")
+        return line_fig, bar_fig
+    else:
+        bar_fig = bar_gender("None")
+        return line_fig, bar_fig
+    
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
